cff0e251d5768bc340a3c28b2c0862547d280d35
lt94 <leetao94cn@gmail.com>
2017-09-28 20:48:47 +0800

    fix:fix update function

--- src/renderer/components/UpdatePage/UpdateBar.vue
+++ src/renderer/components/UpdatePage/UpdateBar.vue
@@ -3,25 +3,15 @@
         <div class="alert alert-info" role="alert">
             当前版本<span class="label label-default">{{ current_version}}</span>可升级到<span class="label label-danger">{{ version }}</span>  <a type="button" class="col-sm-1 col-md-1 col-lg-1 btn btn-danger" style="float:right" v-on:click='updateApp'>升级</a>            
         </div>
-        <div v-if= "updating">
-            <div class="progress">
-                <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em" v-bind:style="{width: progress_width + '%'}">
-                    0%
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 <script>
 import Updates from '@/services/updates'
-import Utils from '@/services/utils'
-import Logs from '@/services/logs'
 
 export default {
   data () {
     return {
       update: false,
-      updating: false,
       current_version: '',
       version: '',
       progress_width: 0
@@ -33,8 +23,7 @@ export default {
   },
   methods: {
     updateApp: function () {
-      this.updating = true
-      Updates.getUpdate().then(this.changeProgrss, this.reportError)
+      Updates.getUpdate()
     },
     changeVersion: function (version) {
       console.log(version)
@@ -46,17 +35,6 @@ export default {
         }
       } else {
         this.$emit('NETSTATUS', false)
-      }
-    },
-    reportError: function (error) {
-      Logs.logError(error)
-      console.log(error)
-    },
-    changeProgrss: function (value) {
-      this.progress_width = value
-      if (value === 100) {
-        this.updating = false
-        Utils.showMessage('更新成功!请重启软件')
       }
     }
   }
--- src/renderer/services/updates/index.js
+++ src/renderer/services/updates/index.js
@@ -1,11 +1,7 @@
 var fs = require('fs')
 var path = require('path')
 var request = require('request')
-var async = require('async')
-var updatefiles = []
-var updatefilesIndex = 0
-var updateCount = 0
-var updatefilesCount = 0
+const {shell} = require('electron')
 
 function getLocalVersion () {
   try {
@@ -33,54 +29,7 @@ function getCurrentVersion () {
 }
 
 function getUpdate () {
-  var versionUrl = 'https://raw.githubusercontent.com/lt94/electron-searchMovies/master/package.json'
-  return new Promise(function (resolve, reject) {
-    request(versionUrl, function (error, response, body) {
-      if (response.statusCode !== 200) {
-        reject(error)
-      } else {
-        var data = JSON.parse(body.toString())
-        if (data.changes === 'undefined') {
-          resolve(100)
-          return
-        }
-        updatefiles = data['changes']
-        updatefilesCount = updatefiles.length
-        updateApp(resolve)
-      }
-    })
-  })
-}
-
-function updateApp (resolve) {
-  var tasks = []
-  for (var i = 0; i < updatefiles.length; i++) {
-    tasks[i] = updateFile()
-  }
-  async.parallel(tasks, function (error, result) {
-    if (error) {
-      console.log(error)
-    }
-    if (result) {
-      updateCount += 1
-      resolve(updateCount / updatefilesCount * 100)
-    } else {
-      updatefilesCount -= 1
-    }
-  })
-}
-
-function updateFile (callback) {
-  var fileUrl = 'https://raw.githubusercontent.com/lt94/electron-searchMovies/master/' + updatefiles[updatefilesIndex++]
-  request(fileUrl, function (error, response, body) {
-    if (response.statusCode !== 200) {
-      console.log(error)
-      callback(error, false)
-    } else {
-      fs.writeFileSync(path.join(__dirname, '../../../../', updatefiles[updatefilesIndex++]))
-      callback(error, true)
-    }
-  })
+  shell.openExternal('https://github.com/lt94/electron-searchMovies/releases')
 }
 
 export default {
